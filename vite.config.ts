a67fe443d54ecf78e4959dd84edad72ac7326ebd
NyTr0g3n3 <112888974+NyTr0g3n3@users.noreply.github.com>
2025-11-02 18:59:25 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Dresscode-AI-V3/',
+    base: '/Dresscode-V3/',
     root: '.',
     build: {
       outDir: 'dist',
