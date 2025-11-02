3c1742d978305cc033899fb19bfc8b45cd5cea6d
NyTr0g3n3 <112888974+NyTr0g3n3@users.noreply.github.com>
2025-11-02 19:04:35 +0100

    Fix casing in base path for Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Dresscode-V3/',
+    base: '/DressCode-V3/',
     root: '.',
     build: {
       outDir: 'dist',
