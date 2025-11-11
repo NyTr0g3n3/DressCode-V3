f88ebc0b7954df4ae25f551da60c01c03f653459
NyTr0g3n3 <112888974+NyTr0g3n3@users.noreply.github.com>
2025-11-11 12:15:48 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header: React.FC<HeaderProps> = ({ theme, toggleTheme, children }) => {
     <header className="bg-snow/80 dark:bg-onyx/80 backdrop-blur-lg border-b border-black/5 dark:border-white/10 sticky top-0 z-50">
       <div className="container mx-auto px-4 lg:px-8 h-24 flex justify-between items-center">
         <h1 className="text-4xl font-serif font-bold tracking-wider text-raisin-black dark:text-snow">
-  Dress <span className="text-gold">Me</span> Up
+  Dress <span className="text-gold">Me</span> Up!
 </h1>
         <div className="flex items-center gap-4">
           {children}
