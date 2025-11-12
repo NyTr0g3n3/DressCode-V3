b2f94a9ba9d73cb56f421755e8aa684cf96a3324
NyTr0g3n3 <112888974+NyTr0g3n3@users.noreply.github.com>
2025-11-12 15:04:12 +0100

    Update VacationModal.tsx

--- src/components/VacationModal.tsx
+++ src/components/VacationModal.tsx
@@ -47,8 +47,12 @@ const VacationModal: React.FC<VacationModalProps> = ({
         </div>
       }
       
-      defaultSnap={({ maxHeight }) => maxHeight * 0.5}
-      snapPoints={({ maxHeight }) => [maxHeight * 0.5, maxHeight * 0.85]}
+      defaultSnap={({ maxHeight }) => maxHeight * 0.6}
+      snapPoints={({ minHeight, maxHeight }) => [
+        minHeight, 
+        maxHeight * 0.6, 
+        maxHeight * 0.85
+      ]}
     >
       <div className="p-6 space-y-6 bg-white dark:bg-raisin-black text-raisin-black dark:text-snow">
         <VacationPlanner 
