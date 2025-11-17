d691ff79dbe273320fd2d62642a30008bbccafd8
NyTr0g3n3 <112888974+NyTr0g3n3@users.noreply.github.com>
2025-11-17 15:43:39 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   geminiApiKey: import.meta.env.VITE_GEMINI_API_KEY || '', 
-  openWeatherApiKey: import.meta.env.VITE_OPENWEATHER_API_KEY || ''
+  openWeatherApiKey: import.meta.env.VITE_OPENWEATHER_API_KEY || '',
+  huggingfaceApiKey: import.meta.env.VITE_HUGGINGFACE_API_KEY || ''
 };
