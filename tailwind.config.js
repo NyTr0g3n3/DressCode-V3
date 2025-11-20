584ea9a4f9fd15846f1a2e94dcba97b9a7a732f2
NyTr0g3n3 <112888974+NyTr0g3n3@users.noreply.github.com>
2025-11-20 23:56:34 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,13 @@ export default {
           '0%': { transform: 'translateY(100%)' },
           '100%': { transform: 'translateY(0)' }
         }
+    shimmer: {
+          '100%': { transform: 'translateX(100%)' },
+        }    
       },
       animation: {
-        'slide-up': 'slide-up 0.3s ease-out'
+        'slide-up': 'slide-up 0.3s ease-out',
+        shimmer: 'shimmer 1.5s infinite'
       }
     },
   },
