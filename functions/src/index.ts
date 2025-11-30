83b4f782ea9f161ff3c78c78ae94a2b7dc81d10d
NyTr0g3n3 <112888974+NyTr0g3n3@users.noreply.github.com>
2025-11-30 22:38:35 +0100

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -43,7 +43,7 @@ export const generateVisualOutfit = onCall(
       logger.info(`Traitement : ${description || "Vetement"} (${category})`);
 
       const output = await replicate.run(
-        "cuuupid/idm-vton",
+        "yisol/idm-vton",
         {
           input: {
             garm_img: garmentUrl,
