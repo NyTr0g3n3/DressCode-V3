09297f323f4a0572dc564f8b0cce9f1f8f6f268d
NyTr0g3n3 <112888974+NyTr0g3n3@users.noreply.github.com>
2025-11-30 20:32:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/DressCode-V3/',
+  base: '/',  
   plugins: [react()],
   resolve: {
     alias: {
